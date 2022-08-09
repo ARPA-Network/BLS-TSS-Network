db7c1e41c7bb9edb24a8f25c2c0a243e72c43bb3
kafeikui <kkffkk3@protonmail.com>
2022-08-09 19:50:25 +0800

    Add logging component (close #2)

--- crates/randcast-node/src/node_client.rs
+++ crates/randcast-node/src/node_client.rs
@@ -1,3 +1,10 @@
+use log::{info, LevelFilter};
+use log4rs::append::console::ConsoleAppender;
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Root};
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::filter::threshold::ThresholdFilter;
+use log4rs::Config as LogConfig;
 use randcast_node::node::context::chain::InMemoryAdapterChain;
 use randcast_node::node::context::context::{Context, InMemoryContext, TaskWaiter};
 use randcast_node::node::context::types::Config;
@@ -20,6 +27,39 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         None => panic!("Didn't get an id_address string"),
     };
 
+    let stdout = ConsoleAppender::builder().build();
+
+    let file = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{d} {l} - {m}{n}")))
+        .build(format!("log/{}/node.log", node_index))
+        .unwrap();
+
+    let err_file = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{d} {l} {t} - {m}{n}")))
+        .build(format!("log/{}/node_err.log", node_index))
+        .unwrap();
+
+    let log_config = LogConfig::builder()
+        .appender(Appender::builder().build("stdout", Box::new(stdout)))
+        .appender(Appender::builder().build("file", Box::new(file)))
+        .appender(
+            Appender::builder()
+                .filter(Box::new(ThresholdFilter::new(LevelFilter::Error)))
+                .build("err_file", Box::new(err_file)),
+        )
+        .build(
+            Root::builder()
+                .appender("stdout")
+                .appender("file")
+                .appender("err_file")
+                .build(LevelFilter::Info),
+        )
+        .unwrap();
+
+    log4rs::init_config(log_config).unwrap();
+
+    // log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+
     let yaml_str = match node_index.parse::<usize>().unwrap() {
         1 => include_str!("../config_test_1.yml"),
         2 => include_str!("../config_test_2.yml"),
@@ -32,15 +72,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let config: Config = serde_yaml::from_str(yaml_str).expect("config.yml read failed!");
 
-    println!("{:?}", config);
+    info!("{:?}", config);
 
     let rng = &mut rand::thread_rng();
 
     let (private_key, public_key) = G1Scheme::keypair(rng);
 
-    println!("dkg private_key: {}", private_key);
-    println!("dkg public_key: {}", public_key);
-    println!("-------------------------------------------------------");
+    info!("dkg private_key: {}", private_key);
+    info!("dkg public_key: {}", public_key);
+    info!("-------------------------------------------------------");
 
     let main_chain_identity = ChainIdentity::new(
         0,
@@ -68,8 +108,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let handle = context.deploy();
 
     // register node to randcast network
-    let mut client =
-        MockControllerClient::new(config.controller_endpoint, config.id_address).await?;
+    let client = MockControllerClient::new(config.controller_endpoint, config.id_address);
 
     client
         .node_register(bincode::serialize(&public_key).unwrap())
