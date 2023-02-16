e2ddef4842f92eae83002504a4c3106374c54350
Andlyn <lyn.sdfz@163.com>
2023-02-16 11:22:31 +0800

    Add keywords to handle contract.

--- tests/scenarios/01__node_registration.robot
+++ tests/scenarios/01__node_registration.robot
@@ -1,10 +1,19 @@
 *** Settings ***
 Documentation       Node Registration Scenarios
 
-Library             environment.local_ethereum
-Resource            common.resource
-
+Library             src/environment/local_ethereum.py
+Resource            src/common.resource
+Resource            src/contract.resource
 
 *** Test Cases ***
 My Test
     Log    RUN
+    Deploy Proxy Contract
+    Get Modified CommitdDkg    ${TEST_NODE_ADDRESS}
+    Set Modified CommitdDkg    ${TEST_NODE_ADDRESS}
+    Get Node    ${TEST_NODE_ADDRESS}
+    Get Group    0
+    Node In Members    0    ${TEST_NODE_ADDRESS}
+    Partial Key Registered    0    ${TEST_NODE_ADDRESS}
+    Get Member    0    0
+    Get Coordinator    0
--- tests/scenarios/src/environment/local_ethereum.py
+++ tests/scenarios/src/environment/local_ethereum.py
@@ -1,8 +1,54 @@
-import os
-import subprocess
-import time
 import json
-
+from web3 import Web3
 
 def some(message):
     print(message)
+
+def get_abi(file_name):
+    #read abi from json file
+    with open(file_name, 'r') as f:
+        abi = json.load(f)
+        abi = abi["abi"]
+    return abi
+
+def get_contract(file_name, address):
+    #get the contract object
+
+    abi = get_abi(file_name)
+    w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+    contract = w3.eth.contract(
+        address = address,
+        abi = abi
+    )
+    return contract
+
+def to_checksum_address(lower_case_address):
+    #Web3.py only accepts checksum addresses
+
+    return Web3.toChecksumAddress(lower_case_address)
+
+def contract_function_call(contract, function_name, *args):
+    # contract is an instance of a web3 contract
+    # function_name is a string representing the name of the function to execute
+    # args is a variable-length argument list representing the parameters for the function
+    
+    # Get the function object from the contract instance
+    function = getattr(contract.functions, function_name)
+    
+    # Call the function with the provided parameters
+    result = function(*args).call()
+    
+    return result
+
+def contract_function_transact(contract, function_name, *args):
+    # contract is an instance of a web3 contract
+    # function_name is a string representing the name of the function to execute
+    # args is a variable-length argument list representing the parameters for the function
+    
+    # Get the function object from the contract instance
+    function = getattr(contract.functions, function_name)
+    
+    # Call the function with the provided parameters
+    result = function(*args).transact()
+    
+    return result
