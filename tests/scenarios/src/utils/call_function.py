f0d42c1301a113a0c9208dbccdf4f40f7a085073
Andlyn <lyn.sdfz@163.com>
2023-08-22 22:29:57 +0800

    Fix issues.

--- tests/scenarios/src/utils/call_function.py
+++ tests/scenarios/src/utils/call_function.py
@@ -15,18 +15,17 @@ def get_abi(file_name):
 
 CONTRACT_ABI = get_abi('contracts/out/AdvancedGetShuffledArrayExampleTest.sol/AdvancedGetShuffledArrayExampleTest.json')
 
-# Replace the placeholder below with your contract address
-CONTRACT_ADDRESS = '0xa3Fc37b87CbB7D12f7b8d650B544Af1A7B1985AE'
+# Replace with your contract address
+CONTRACT_ADDRESS = 'xxx'
 
 def main():
-    # Connect to Sepolia Testnet via Infura
-    w3 = Web3(Web3.HTTPProvider(f'https://eth-sepolia.g.alchemy.com/v2/7RFds6TGJiIN5gR4BHrEtiMjJRvV44Na'))
-
-    # Set the defalut account from private key
-    w3.eth.default_account = '0x6a129A0C886bC8Ac6D0b4e1C3D460498345d534d'
+    # Connect to Sepolia
+    w3 = Web3(Web3.HTTPProvider('https://eth-sepolia.g.alchemy.com/v2/xxx'))
+    # Set the defalut account
+    w3.eth.default_account = ''
     # Set up the contract instance
     contract_instance = w3.eth.contract(address=Web3.to_checksum_address(CONTRACT_ADDRESS), abi=CONTRACT_ABI)
-
+    # Call the function
     result = contract_instance.functions.getRandomNumberThenGenerateShuffledArray(10, 2, 42, 0, 350000, 1000000000).transact()
     tx_receipt = w3.eth.wait_for_transaction_receipt(result)
 
